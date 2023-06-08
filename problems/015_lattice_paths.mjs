fae18fa07af72eedbaccfddbdce2db662ba4799d
coursera <kassi.khalid9@gmail.com>
2023-06-08 23:05:53 +0100

    salam

---
+++ problems/015_lattice_paths.mjs
@@ -0,0 +1,8 @@
+/*
+Euler Project Problem 15 : Lattice Path | Javascript | Khalid9ASSI
+
+Starting in the top left corner of a 2x2 grid, and only being able to move to the right and down, there are exactly routes to the bottom right corner.
+
+How many such routes are there through a 20 x 20 grid ?
+
+*/
